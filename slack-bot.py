cc5339140f41ab1680ba10d9dca116b004e7895a
arvehisa <arvehisa@gmail.com>
2023-10-16 23:49:48 +0900

    environ

--- slack-bot.py
+++ slack-bot.py
@@ -5,7 +5,7 @@ SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
 
 # Boltアプリの初期化
-app = App(token="SLACK_BOT_TOKEN", signing_secret="SLACK_SIGNING_SECRET")
+app = App(token=SLACK_BOT_TOKEN, signing_secret=SLACK_SIGNING_SECRET)
 
 # app_mention イベントのリッスン
 @app.event("app_mention")
