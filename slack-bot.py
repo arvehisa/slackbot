1267b5bba3c0402f7d22993182593c769cde25b2
arvehisa <arvehisa@gmail.com>
2023-10-16 23:04:16 +0900

    first commit

---
+++ slack-bot.py
@@ -0,0 +1,14 @@
+from slack_bolt import App
+
+# Boltアプリの初期化
+app = App(token="YOUR_SLACK_BOT_TOKEN", signing_secret="YOUR_SLACK_SIGNING_SECRET")
+
+# app_mention イベントのリッスン
+@app.event("app_mention")
+def mention(event, say):
+    # メンションされた際の反応
+    say("こんにちは")
+
+# サーバーの起動
+if __name__ == "__main__":
+    app.start(port=3000)
