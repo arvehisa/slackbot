323dc50961e0505536e23b6ec9981710882768c8
arvehisa <arvehisa@gmail.com>
2023-10-19 00:31:11 +0900

    openai, bedrock, reply in thread

--- slack-bot-bedrock.py
+++ slack-bot-bedrock.py
@@ -4,7 +4,7 @@ import re
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-from langchain.llms import OpenAI
+from langchain.llms import Bedrock
 # from langchain.prompts import PromptTemplate
 
 # Slackbot
@@ -13,7 +13,14 @@ SOCKET_MODE_TOKEN = os.environ['SOCKET_MODE_TOKEN']
 app = App(token=SLACK_BOT_TOKEN)
 
 # llm
-llm = OpenAI()
+llm = Bedrock(
+    model_id="anthropic.claude-v2",
+)
+
+#prompt = PromptTemplate(
+#    input_variables = ["no_mention_text"]
+#    template = f"""please answer step by step. {no_mention_text} Answer: """
+#)
 
 @app.event("app_mention")
 def mention(event, say):
@@ -21,7 +28,7 @@ def mention(event, say):
     no_mention_text = re.sub(r'^<.*>', '', event['text'])
     thread_ts = event['ts']
 
-    template = f"""Question: please translate Japanese English. {no_mention_text} Answer: """
+    template = f"""Question: please answer step by step. {no_mention_text} Answer: """
     response = llm(template)
     
     say(text=response, thread_ts=thread_ts)
@@ -30,3 +37,4 @@ def mention(event, say):
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SOCKET_MODE_TOKEN)
     handler.start()
+
